c28fa4f4597596d4710dc6d388c17426c0240574
Sambuddha Chatterjee <sambuddha.c30@gmail.com>
2024-04-01 22:57:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import cufflinks as cf
 from sklearn.metrics import accuracy_score
+import json
+
 @st.cache_data()
 def load_model_transformer():
     #loading the transformer model imported from hugging face
@@ -140,19 +142,72 @@ def model_implementation_transformer():
     newdf['sentiment']=newdf['reviewText'].apply(lambda x: sentiment_score(x[:512]))
     return newdf
 
+@st.cache_data()
+def model_implementation_LSTM():
+    from tensorflow import keras
+    from tensorflow.keras import layers
+    from sklearn.model_selection import train_test_split
+    df1 = load_data_init()
+    max_features = 20000  # Only consider the top 20k words
+    maxlen = 200  # Only consider the first 200 words of each movie revie
+    # Input for variable-length sequences of integers
+    inputs = keras.Input(shape=(None,), dtype="int32")
+    # Embed each integer in a 128-dimensional vector
+    x = layers.Embedding(max_features, 128)(inputs)
+    # Add 2 bidirectional LSTMs
+    x = layers.Bidirectional(layers.LSTM(64, return_sequences=True))(x)
+    x = layers.Bidirectional(layers.LSTM(64))(x)
+    # Add a classifier
+    outputs = layers.Dense(1, activation="sigmoid")(x)
+    model = keras.Model(inputs, outputs)
+    from keras.preprocessing.text import Tokenizer
+    # Initialize a tokenizer
+    tokenizer = Tokenizer(num_words=max_features)
+    # Fit the tokenizer on your text data
+    X = df1['reviewText']
+    tokenizer.fit_on_texts(X)
+    # Convert the text data into sequences of integers
+    X_sequences = tokenizer.texts_to_sequences(X)
+    # Split the data into training and test sets
+    df1['custom_overall'] = df1['overall'].apply(lambda x: 1 if x > 2 else 0)
+    Y = df1['custom_overall']
+    x_train, x_test, y_train, y_test = train_test_split(X_sequences, Y, test_size=0.2, random_state=42)
+    # Pad the sequences
+    x_train = keras.preprocessing.sequence.pad_sequences(x_train, maxlen=maxlen)
+    x_test = keras.preprocessing.sequence.pad_sequences(x_test, maxlen=maxlen)
+    model.compile("adam", "binary_crossentropy", metrics=["accuracy"])
+    history = model.fit(x_train, y_train, batch_size=32, epochs=10, validation_data=(x_test, y_test))
+    model.save('my_model.h5')  # Creates a HDF5 file 'my_model.h5'
+    history_dict = history.history
+    with open('history.json', 'w') as f:
+        json.dump(history_dict, f)
+    return history,model
+
 def model_comparison():
+    from tensorflow import keras
+    from keras.preprocessing.text import Tokenizer
+    from keras.preprocessing.sequence import pad_sequences
+    from keras.models import load_model
+    max_features = 20000  # Only consider the top 20k words
+    maxlen = 200  # Only consider the first 200 words of each movie review
+    
+    tokenizer = Tokenizer(num_words=max_features)
     df1,df2 = load_data()
+    st.subheader("Assumptions")
+    st.write(" 1) The overall feature contains the actual values of the ratings.")
+    st.write(" 2) Positive vs Negative Classification: Classifying reviews with 4-5 stars as positive and 1-3 stars as negative.")    
     st.subheader("Sentiment Analysis using nltk.sentiment.vader")
     st.write("Details :")
         # Create two columns
+    df1['custom_overall'] = df1['overall'].apply(lambda x: 'Positive' if x > 3 else 'Negative')
     col1, col2 = st.columns(2)
     # Display value counts in the first column
     col1.subheader("Overall Rating Counts")
-    col1.write(df1['overall'].value_counts())
+    col1.write(df1['custom_overall'].value_counts())
     # Display value counts in the second column
     col2.subheader("Sentiment Value Counts")
     col2.write(df1['sentiment'].value_counts())
-    #st.subheader("Accuracy of the Model")
+    df1['custom_overall'] = df1['overall'].apply(lambda x: 'Positive' if x > 3 else 'Negative')
     #df[df["sentiment"]=='Positive'].sort_values("wilson_lower_bound",ascending=False).head(5)
     # Assuming df['overall'] contains the actual values and df['sentiment'] contains the predicted values
     #accuracy = accuracy_score(df['overall'], df['sentiment'])
@@ -163,10 +218,10 @@ def model_comparison():
     st.write("Details :")
     # Create two columns
     col1, col2 = st.columns(2)
-    col1.subheader("Calcualted Sentiment")
-    col1.write(df2['sentiment'].value_counts())
-    col2.subheader("Actual Rating Counts")
-    col2.write(df2['overall'].value_counts())
+    col2.subheader("Calcualted Sentiment")
+    col2.write(df2['sentiment'].value_counts())
+    col1.subheader("Actual Rating Counts")
+    col1.write(df2['overall'].value_counts())
 
     st.subheader("Accuracy of the Model")
     st.write("Assuming the overall feature contains the actual values comparing with calculated sentiment containing the predicted values")
@@ -182,7 +237,6 @@ def model_comparison():
     X2 = df2['overall'].value_counts()
     labels = ['Excellent', 'Good', 'Neutral', 'Bad', 'Worse']
     colors = ['green', 'lightgreen', 'yellow', 'orange', 'red']
-    
     axs[0].pie(X1, labels=labels, colors=colors, autopct='%1.1f%%')
     axs[0].set_title('Calculated Ratings')
 
@@ -191,9 +245,67 @@ def model_comparison():
 
     plt.legend(title="Calculated Ratings", bbox_to_anchor=(1,1))
     plt.tight_layout()
-
+    st.pyplot(fig)
+    st.subheader("Sentiment Analysis using LSTM")
+    st.write("Details :")
+    model = load_model('my_model.h5')
+    with open('history.json', 'r') as f:
+        loaded_history = json.load(f)
+    history = loaded_history
+    # Plot the training and validation accuracy
+    st.subheader('Training and Validation Accuracy')
+    fig, ax = plt.subplots()
+    ax.plot(history['accuracy'], label='Train Accuracy')
+    ax.plot(history['val_accuracy'], label='Validation Accuracy')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Accuracy')
+    ax.legend()
     st.pyplot(fig)
 
+    # Plot the training and validation loss
+    st.subheader('Training and Validation Loss')
+    fig, ax = plt.subplots()
+    ax.plot(history['loss'], label='Train Loss')
+    ax.plot(history['val_loss'], label='Validation Loss')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Loss')
+    ax.legend()
+    st.pyplot(fig)
+    df = load_data_init()
+    df_n = df.iloc[:100,1:3]
+    review_sequences = tokenizer.texts_to_sequences(df_n['reviewText'])
+    review_padded = pad_sequences(review_sequences, maxlen=maxlen)
+    # Predict the sentiment of the reviews
+    st.subheader("Accuracy of the Model for the first 100 rows")
+    df_n['overall_custom'] = df_n['overall'].apply(lambda x: 1 if x > 3 else 0)
+    df_n['calculated'] = model.predict(review_padded)
+    df_n['calculated_rating'] = df_n['calculated'].apply(lambda x: 1 if x > 0.5 else 0)
+    st.write("Comparison of Actual and Calculated Ratings")
+    col1, col2 = st.columns(2)
+    col1.subheader("Actual Ratings")
+    col1.write(df_n['overall_custom'].value_counts())
+    col2.subheader("Calculated Ratings")
+    col2.write(df_n['calculated_rating'].value_counts())
+
+    accuracy = accuracy_score(df_n['overall_custom'], df_n['calculated_rating'])
+    st.write(f"Accuracy: {accuracy * 100:.2f}%")
+    st.header("Sample implementation of the model")
+    # Get the review from the user
+    review = st.text_input("Enter your review", "Type Here")
+    if st.button("Submit"):
+        # Convert the review to a sequence of integers
+        review_sequence = tokenizer.texts_to_sequences([review])
+        # Pad the sequence
+        review_padded = keras.preprocessing.sequence.pad_sequences(review_sequence, maxlen=maxlen)
+        # Predict the sentiment of the review
+        sentiment = model.predict(review_padded)
+        # Write the sentiment to the screen
+        #st.write("Sentiment of the review is: ", sentiment)
+        if(sentiment>0.5):
+            st.write("The review is Positive")
+        else:
+            st.write("The review is Negative")
+        
 def main():
     st.sidebar.title("Navigation")
     page = st.sidebar.radio("Go to", ["Home", "Data Analysis", "Model Comparison"])
